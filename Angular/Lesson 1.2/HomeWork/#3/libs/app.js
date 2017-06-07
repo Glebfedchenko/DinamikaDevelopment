c5a962387165bf38292a987f82d83742d0a9ee93
GlebFedchenko <fedchenkogleb@gmail.com>
2017-06-07 20:46:57 +0300

    homework_task 2

--- Angular/Lesson 1.2/HomeWork/#3/libs/app.js
+++ Angular/Lesson 1.2/HomeWork/#3/libs/app.js
@@ -1,10 +1,12 @@
 var app = angular.module('app', []);
 
 app.controller('mainCtrl', function ($scope) {
-    $scope.text = "";
 
-    $scope.addText = function(){
-        $scope.withText = $scope.text;
-    }
+    $scope.q1 = 'Q1.html';
+    $scope.q2 = 'Q2.html';
+    $scope.q3 = 'Q3.html';
+    $scope.q4 = 'Q4.html';
+    $scope.q5 = 'Q5.html';
 
+    $scope.url1 = $scope.q1;
 });
