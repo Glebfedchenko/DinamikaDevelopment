f5d0038c9c9c6175f589123561fefbd55497133a
GlebFedchenko <fedchenkogleb@gmail.com>
2017-06-08 00:23:10 +0300

    Lesson 2 homework task3

--- Angular/Lesson 1.2/ClassWork/#3/libs/app.js
+++ Angular/Lesson 1.2/ClassWork/#3/libs/app.js
@@ -1,10 +1,34 @@
 var app = angular.module('app', []);
 
 app.controller('mainCtrl', function ($scope) {
+
+    $scope.nameUrl = 'libs/name.html';
+    $scope.priceUrl = '';
+    $scope.categoryUrl = '';
+    $scope.countUrl = '';
+    $scope.taxUrl = '';
+    $scope.expirationUrl = '';
     $scope.items = [
         { name: "B Item", price: 10.9, category: "Category 1", count: 10, tax: 1.12, expiration: 10 },
         { name: "A Item", price: 1.1, category: "Category 1", count: 8, tax: 0.55, expiration: 12 },
         { name: "D Item", price: 2.6, category: "Category 2", count: 7, tax: 0.22, expiration: 5 },
         { name: "C Item", price: 17.5, category: "Category 2", count: 33, tax: 2.77, expiration: 10 }];
-
+    $scope.nameShowTable = function () {
+        $scope.nameUrl = 'name.html';
+    };
+    $scope.priceShowTable = function () {
+        $scope.priceUrl = 'price.html';
+    };
+    $scope.categoryShowTable = function () {
+        $scope.categoryUrl = 'category.html';
+    };
+    $scope.countShowTable = function () {
+        $scope.countUrl = 'count.html';
+    };
+    $scope.taxShowTable = function () {
+        $scope.taxUrl = 'tax.html';
+    };
+    $scope.expirationShowTable = function () {
+        $scope.expirationUrl = 'expiration.html';
+    };
 });
--- Angular/Lesson 1.2/HomeWork/#3/libs/app.js
+++ Angular/Lesson 1.2/HomeWork/#3/libs/app.js
@@ -2,11 +2,33 @@ var app = angular.module('app', []);
 
 app.controller('mainCtrl', function ($scope) {
 
-    $scope.q1 = 'Q1.html';
-    $scope.q2 = 'Q2.html';
-    $scope.q3 = 'Q3.html';
-    $scope.q4 = 'Q4.html';
-    $scope.q5 = 'Q5.html';
+    $scope.mode = {
+            name: 'Choose a question'
+        };
+        $scope.count1 = 0;
+        $scope.count2 = 0;
+        $scope.count3 = 0;
+        $scope.count4 = 0;
+        $scope.count5 = 0;
+        $scope.total = 0;
 
-    $scope.url1 = $scope.q1;
-});
+        
+        $scope.result1 = function () {
+            $scope.count1 = 1;
+        };
+        $scope.result2 = function () {
+            $scope.count2 = 1;
+        };
+        $scope.result3 = function () {
+            $scope.count3 = 1;
+        };
+        $scope.result4 = function () {
+            $scope.count4 = 1;
+        };
+        $scope.result5 = function () {
+            $scope.count5 = 1;
+        };
+        $scope.showTotal = function () {
+            $scope.all = $scope.count1 + $scope.count2 + $scope.count3 + $scope.count4 + $scope.count5;
+        }
+    });
