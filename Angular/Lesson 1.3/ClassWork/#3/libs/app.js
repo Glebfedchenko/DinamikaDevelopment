7917306efcc6e86ffcb44c9db243196347ac251c
Gleb Fedchenko <fedchenkogleb@gmail.com>
2017-06-08 10:29:39 +0300

    Create app.js

--- Angular/Lesson 1.3/ClassWork/#3/libs/app.js
+++ Angular/Lesson 1.3/ClassWork/#3/libs/app.js
@@ -7,6 +7,6 @@ app.controller('mainCtrl', function ($scope) {
              { name: "D Item", price: 2.6, category: "Category 2", count: 7, tax: 0.22, expiration: 5 },
              { name: "C Item", price: 17.5, category: "Category 2", count: 33, tax: 2.77, expiration: 10 }];
   $scope.expSort = function(item){
-    return item.expiration >=10;
+    return item.expiration > 10;
   };
 });
