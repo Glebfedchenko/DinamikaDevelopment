648d93cdb0773be5feee42c7e447e81d39f5e519
GlebFedchenko <fedchenkogleb@gmail.com>
2017-06-08 17:27:34 +0300

    Lesson 3 homework #3

--- Angular/Lesson 1.3/HomeWork/#3/libs/app.js
+++ Angular/Lesson 1.3/HomeWork/#3/libs/app.js
@@ -1,6 +1,32 @@
 var app = angular.module('app', []);
 
 app.controller('mainCtrl', function ($scope) {
-   
+
+    $scope.getError = function(error){
+        if(angular.isDefined(error)){
+            if(error.required){
+                return "Field doesn't have to be empty";
+            }
+        }
+    }
+
+    $scope.mailGetError = function (error) {
+        if (angular.isDefined(error)) {
+            if (error.required) {
+                return "Field doesn't have to be empty";
+            } else if (error.email) {
+                return "Enter correct E-mail";
+            }
+        }
+    }
+    $scope.valid = false;
+    $scope.changepass = function(){
+        var regexp = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-z0-9!@#$%^&]{8,12}$/;
+        if(regexp.test($scope.password)==false){
+            $scope.passvalidation = false;
+        } else{
+            $scope.passvalidation=true;
+        }
+    };
 
 });
