581bc9e46cfa3322c748827de1f7f22f1f1a5de4
GlebFedchenko <fedchenkogleb@gmail.com>
2017-06-08 18:11:35 +0300

    Completed Lesson 1.3

--- Angular/Lesson 1.3/HomeWork/#2/libs/app.js
+++ Angular/Lesson 1.3/HomeWork/#2/libs/app.js
@@ -2,5 +2,18 @@ var app = angular.module('app', []);
 
 app.controller('mainCtrl', function ($scope) {
    
+    $scope.animals=  [
+       {name:'dog' },
+       {name:'cat'  },
+       {name:'frog'  },
+       {name:'chinchilla'  }
+     ]
+
+    $scope.search=function(){
+        $scope.searchQuery = angular.copy($scope.query);
+        $scope.animalsToFilter=$scope.animals;
+        $scope.searchReasult=true;
+
+    }
 
 });
