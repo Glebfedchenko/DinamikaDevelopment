b5d766cf42c39f3a7910d1fe6a9fa1f875faf99f
GlebFedchenko <fedchenkogleb@gmail.com>
2017-06-17 01:38:37 +0300

    Fixed issue #8
    Added NaN & Infinity exception handlers

--- Angular/Lesson 5/ClassWork/Task 1/script.js
+++ Angular/Lesson 5/ClassWork/Task 1/script.js
@@ -1,12 +1,16 @@
 var app = angular.module('app', [])
-.controller('mainCtrl',function($scope,$parse){
-    $scope.calc = '';
-    const calculator = {x1:'',x2:''};
-    
-    $scope.parseCalc = function(){
-        var fn = $parse($scope.calc);
-        $scope.result = fn(calculator);
-    }
+    .controller('mainCtrl', function ($scope, $parse) {
+        $scope.calc = '';
+        const calculator = { x1: '', x2: '' };
 
-
-});
+        $scope.parseCalc = function () {
+            var fn = $parse($scope.calc);
+            $scope.result = fn(calculator);
+            if ($scope.result === Infinity) {
+                alert('Divizion By Zero is impossible');
+            }
+            if ($scope.result === NaN) {
+                alert('Enter a correct number');
+            }
+        }
+    });
