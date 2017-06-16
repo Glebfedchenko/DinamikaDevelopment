96399d6844b06a487dce9d77642a9d8b3bdc0b56
GlebFedchenko <fedchenkogleb@gmail.com>
2017-06-17 02:18:13 +0300

    Fix to issue #10
    1. Added more text
    2. Fixed tags
    3.Added angular-scroll service instead of jquery

--- Angular/Lesson 5/HomeWork/Task 3/script.js
+++ Angular/Lesson 5/HomeWork/Task 3/script.js
@@ -1,10 +1,80 @@
-angular.module('app', []).
-    controller('mainCtrl', function ($scope, $location, $anchorScroll) {
-        $scope.scrollTo = function (scrollLocation) {
-            $location.hash(scrollLocation);
-            $anchorScroll();
-        }
+angular.module('app', ['duScroll']).
+    controller('mainCtrl', function ($scope, $location, $anchorScroll, $timeout, $document) {
 
-        $scope.header = ['Home', 'About', 'Hire','Learn'];
+        $scope.data = [
+            {
+                id: '1',
+                header: 'Fish',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '2',
+                header: 'Cheese',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '3',
+                header: 'Meat',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '4',
+                header: 'Vegetables',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '5',
+                header: 'Species',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '6',
+                header: 'Water',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '7',
+                header: 'Wind',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '8',
+                header: 'Oil',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '9',
+                header: 'Ground',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '10',
+                header: 'Fire',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '11',
+                header: 'Tzunami',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+            {
+                id: '12',
+                header: 'Acid',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid assumenda quas quia illo ut, inventore non saepe vitae perferendis tempore et alias officia quis dignissimos commodi, amet quisquam nisi. Aut.'
+            },
+        ];
+
+        var top = 400;
+        var duration = 2000;
+        $timeout(function () {
+            $location.hash('');
+            $anchorScroll();
+        });
+
+        $scope.go = function (id) {
+            var offset = 30;
+            var someElement = angular.element(document.getElementById(id));
+            $document.scrollToElement(someElement, offset, duration);
+
+        }
     });
-$('.a').locallScroll();
