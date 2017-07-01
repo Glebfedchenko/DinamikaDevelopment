73d92a0d23322570b96c05af022be5074e56c4f9
GlebFedchenko <fedchenkogleb@gmail.com>
2017-07-02 01:44:15 +0300

    homework#1 Lesson 21

--- React/Lesson_21/HomeWork/task_1/src/App.js
+++ React/Lesson_21/HomeWork/task_1/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+constructor(){
+    super();
+    this.state ={
+        count:0
+    }
+    this.handleDown = this.handleDown.bind(this);
+    this.handleUp = this.handleUp.bind(this);
+}
+
+     handleUp(){
+         this.setState({count:this.state.count + 1})
+     }
+     handleDown(){
+         this.setState({count:this.state.count - 1})
+     }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count}</h1>
+                <button className="btn btn-primary" onClick={this.handleUp}>+</button>
+                <button className="btn btn-danger" onClick={this.handleDown}>-</button>
+            </div>
+        );
+    }
 }
 
 export default App;
