39c14d3396b0e92d44a71962e931de1a5e93b978
GlebFedchenko <fedchenkogleb@gmail.com>
2017-07-05 00:11:07 +0300

    fixes to promise

--- React/Lesson_22/ClassWork/task_2/src/App.js
+++ React/Lesson_22/ClassWork/task_2/src/App.js
@@ -4,20 +4,33 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            src:'',
+            src: "",
+            get: './image.jpg'
         }
     }
-    load(){
-        return new Promise(function(resolve,reject){
-            resolve();
-            reject();
-        })
+    getImage() {
+        let url = this.state.get;
+        return new Promise(function (resolve, reject) {
+            var img = new Image();
+            img.onload = function () {
+                return resolve(url);
+            }
+            img.onerror = function () {
+                return reject(alert("Failed to load image"));
+            }
+            img.src = url;
+        }).then(
+            this.setState({
+                src: url
+            })
+        );
     }
     render() {
         return (
             <div>
-            <button onClick={this.getImage}>Get Image</button>
-        </div>
+                <button onClick={this.getImage.bind(this)}>Get Image</button>
+                <img src={this.state.src} alt="" />
+            </div>
         );
     }
 }
