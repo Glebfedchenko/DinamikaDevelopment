3357b2eb959b37ce12e7d5aef788fe6245934e97
GlebFedchenko <fedchenkogleb@gmail.com>
2017-07-09 22:07:19 +0300

    fixed #19
    added validation

--- React/Lesson_21/HomeWork/task_3/src/Form.js
+++ React/Lesson_21/HomeWork/task_3/src/Form.js
@@ -3,53 +3,119 @@ import React, { Component } from 'react';
 class Form extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            name: '',
-            email: '',
-            phone: ''
-        }
+            username: '',
+            password: '',
+            tel: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleEmailChange = this.handleEmailChange.bind(this);
     }
-    handleSubmit(event) {
-        event.preventDefault();
-        console.log('Form Submitted');
+
+    handleChange(e) {
+        e.target.classList.add('active');
+
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+
+        this.showInputError(e.target.name);
     }
-    handleEmailChange(event) {
-        this.setState({ email: event.target.value })
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        console.log('component state', JSON.stringify(this.state));
+
+        if (!this.showFormErrors()) {
+            console.log('form is invalid: do not submit');
+        } else {
+            console.log('form is valid: submit');
+        }
+    }
+
+    showFormErrors() {
+        const inputs = document.querySelectorAll('input');
+        let isFormValid = true;
+
+        inputs.forEach(input => {
+            input.classList.add('active');
+
+            const isInputValid = this.showInputError(input.name);
+
+            if (!isInputValid) {
+                isFormValid = false;
+            }
+        });
+
+        return isFormValid;
+    }
+
+    showInputError(refName) {
+        const validity = this.refs[refName].validity;
+        const label = document.getElementById(`${refName}Label`).textContent;
+        const error = document.getElementById(`${refName}Error`);
+        const isPassword = refName.indexOf('password') !== -1;
+        const isPasswordConfirm = refName === 'tel';
+
+        if (!validity.valid) {
+            if (validity.valueMissing) {
+                error.textContent = `${label} is a required field`;
+            } else if (validity.typeMismatch) {
+                error.textContent = `${label} should be a valid email address`;
+            } else if (isPassword && validity.patternMismatch) {
+                error.textContent = `${label} should be longer than 4 chars`;
+            } else if (isPasswordConfirm && validity.patternMismatch) {
+                error.textContent = 'Please input phone correctly';
+            }
+            return false;
+        }
+
+        error.textContent = '';
+        return true;
     }
     render() {
         return (
-            <form onSubmit={this.handleSubmit} className='text-center'>
-                <div>
-                    <label htmlFor="Name">Name</label>
+            <form novalidate>
+                <div className="form-group">
+                    <label id="passwordLabel">Username</label>
+                    <input className="form-control"
+                        type="password"
+                        name="password"
+                        ref="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        pattern=".{5,}"
+                        required />
+                    <div className="error" id="passwordError" />
                 </div>
-                <input
-                    id='Name'
-                    type="text"
-                    placeholder="Name"
-                    value={this.state.name} />
-                <div>
-                    <label htmlFor="Email">Email</label>
+                <div className="form-group">
+                    <label id="usernameLabel">E-mail</label>
+                    <input className="form-control"
+                        type="email"
+                        name="username"
+                        ref="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                        required />
+                    <div className="error" id="usernameError" />
                 </div>
-                <input
-                    id='Email'
-                    type="text"
-                    placeholder="Email"
-                    value={this.state.email}
-                    onChange={this.handleEmailChange} />
-                <div>
-                    <label htmlFor="Email">Phone Number</label>
+                <div className="form-group">
+                    <label id="passwordConfirmLabel">Phone Number</label>
+                    <input className="form-control"
+                        type="password"
+                        name="passwordConfirm"
+                        ref="passwordConfirm"
+                        value={this.state.passwordConfirm}
+                        onChange={this.handleChange}
+                        pattern="\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}"
+                        required />
+                    <div className="error" id="passwordConfirmError" />
                 </div>
-                <input
-                    id='Email'
-                    type="text"
-                    placeholder="Email"
-                    value={this.state.phone}
-                    onChange={this.handleEmailChange} />
-                <div>
-                    <button>Submit</button>
-                </div>
+                <button className="btn btn-primary"
+                    onClick={this.handleSubmit}>submit</button>
             </form>
         );
     }
