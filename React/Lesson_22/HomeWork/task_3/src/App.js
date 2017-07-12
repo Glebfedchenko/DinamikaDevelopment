3bf9a10b4c6dbe23c2e7f82142d6e2aaeececf99
GlebFedchenko <fedchenkogleb@gmail.com>
2017-07-13 01:05:25 +0300

    fixed #20

--- React/Lesson_22/HomeWork/task_3/src/App.js
+++ React/Lesson_22/HomeWork/task_3/src/App.js
@@ -1,69 +1,78 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import UserTable from './User';
 
 class Person {
-    constructor(firstName = 'John', lastName = 'Doe', age = 0, gender = 'Male') {
-        this.firstName = firstName,
-            this.lastName = lastName,
-            this.age = age,
-            this.gender = gender
+    constructor(firstName, lastName, age, gender) {
+        [this.firstName = "John", this.lastName = "Doe", this.age = 0, this.gender = "Male"] = [...arguments];
     }
 
-    fullName() {
-        let a = this.firstName + ' ' + this.lastName;
-        return a;
-    }
+    get fullName() {return this.firstName + ' ' + this.lastName;}
+
     sayHi() {
-        return 'Hello, my name is' + ' ' + this.fullName();
+        alert ('Hello, my name is ' + this.fullName);
     }
 }
 
-const person = new Person();
-console.log(person.sayHi());
 
 class User extends Person {
-    constructor(firstName, lastName, age, gender, signUpDate, id) {
-        super(firstName, lastName, age, gender);
-        this.signUpDate = signUpDate,
-            this.id = id;
-    }
-    click() {
-        users.map((user, index) => {
-            return (
-                <div>
-                    {user.sayHi()}
-                </div>
-            );
-        })
+    constructor(signUpDate, id) {
+        super ();
+        [this.signUpDate = 0, this.id] = [...arguments];
     }
 }
 
-let user1 = null;
-let user2 = null;
-let user3 = null;
-
-const users = [
-    user1 = new User('Elvis', 'Prestley', 99, 'Male', Date.now(), 1),
-    user2 = new User('Bugs', 'Bunny', 22, 'Male', Date.now(), 2),
-    user3 = new User('Lola', 'Bunny', 20, 'Female', Date.now(), 3)
-];
-console.log(users);
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                {users.map((user, index) => {
-                    return (
-                        <div key={index}>
-                            <p >FirstName : {user.firstName} </p>
-                            <p>LastName : {user.lastName}</p>
-                            <p>Age: {user.age}</p>
-                            <hr />
-                        </div>
-                    )
-                })}
-            </div>
 
+
+    createUsers() {
+
+        let users = [];
+        users[0] = new User ('12/08/2013', '12');
+        users[1] = new User ('14/09/2014', '10');
+        users[2] = new User ('16/10/2015', '8');
+
+
+        Object.assign(users[0], {
+            firstName: 'Bugs',
+            lastName: 'Bunny',
+            age: '99',
+            gender: 'male'
+        });
+
+        Object.assign(users[1], {
+            firstName: 'Lola',
+            lastName: 'Bunny',
+            age: '99',
+            gender: 'female'
+
+        });
+        Object.assign(users[2], {
+            firstName: 'Lion',
+            lastName: 'King',
+            age: '100',
+            gender: 'male'
+
+        });
+
+        return users
+    }
+
+    componentWillMount() {
+        this.users = this.createUsers ();
+    }
+
+
+    render() {
+
+        return (
+            <div className="container">
+                <div className="panel panel-primary">
+                    <div className="panel-body">
+                        <UserTable users={this.users}/>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
