cae09d1aa7bc509bbdd47549d9852011671d8ffc
Gleb Fedchenko <fedchenkogleb@gmail.com>
2017-08-08 15:29:35 +0300

    fixed #25
    fixed

--- Node/lesson_27/ClassWork/task_3/task_3.js
+++ Node/lesson_27/ClassWork/task_3/task_3.js
@@ -1,9 +1,7 @@
-/*1st way*/
-var a = require('./test/test');
-a();
-/*2nd way*/
-var a = require('./test');
-a();
-/*3rd way*/
-var a =require.resolve('/lesson_27/ClassWork/task_3/test/test.js');
-a();
+const test1 = require('./test'); //a file to require is specified in root directory
+const test2 = require('./test1/test1'); //a file to require is directly specified 
+const test3 = require('./test2'); //looks for a file within given directory
+
+test1();
+test2();
+test3();
